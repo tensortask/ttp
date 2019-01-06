be4bf86800dbdfe82d128561376d43a8bf390b85
sbsends <sbsends@gmail.com>
2019-01-06 14:22:28 -0800

    go: moved gogo optimized protos to go directory
    This is generally a cleaner layout with and allows the golang import path to be more idiomatic.

--- go/protos/generate.go
+++ go/protos/generate.go
@@ -22,19 +22,23 @@ func main() {
 }
 
 func generateProtos() error {
+	err := os.Chdir("..")
+	if err != nil {
+		return err
+	}
 	args := []string{
 		"-I=protos",
 		fmt.Sprintf("-I=%s", filepath.Join(goPath, "src")),
 		fmt.Sprintf("-I=%s", filepath.Join(goPath, "src", "github.com", "gogo", "protobuf", "protobuf")),
 		fmt.Sprintf("--proto_path=%s", filepath.Join(goPath, "src", "github.com")),
-		"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:gen",
+		"--gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:.",
 		"tensor.proto",
 		"transport.proto",
 		"type.proto",
 	}
 	cmd := exec.Command("protoc", args...)
 	fmt.Println(cmd)
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return err
 	}
